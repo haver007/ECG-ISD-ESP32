b7d20ca889c3c1ed015466028b94a46294b3a772
Maximilian Haverkamp <technohaver@yahoo.de>
2019-11-20 23:17:02 +0100

    cleanup main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "onScreen.h"
 //declare Tasks
 void taskScreen(void *parameter);
-void taskTwo(void *parameter);
+
 //Declare Objects
 setupWiFi *crtlWiFi;
 onScreen *crtlScreen;
@@ -22,18 +22,10 @@ void setup()
   xTaskCreate(
       taskScreen,   /* Task function. */
       "TaskScreen", /* String with name of task. */
-      5000,        /* Stack size in bytes. */
+      5000,         /* Stack size in bytes. */
       NULL,         /* Parameter passed as input of the task */
       1,            /* Priority of the task. */
       NULL);        /* Task handle. */
-
-  xTaskCreate(
-      taskTwo,   /* Task function. */
-      "TaskTwo", /* String with name of task. */
-      10000,     /* Stack size in bytes. */
-      NULL,      /* Parameter passed as input of the task */
-      1,         /* Priority of the task. */
-      NULL);     /* Task handle. */
 }
 
 void loop()
@@ -45,19 +37,4 @@ void loop()
 void taskScreen(void *parameter)
 {
   crtlScreen->loop();
-}
-
-void taskTwo(void *parameter)
-{
-
-  for (int i = 0; i < 10; i++)
-  {
-
-    Serial.println("Hello from task 2");
-    Serial.print("loop() running on core ");
-    Serial.println(xPortGetCoreID());
-    delay(1000);
-  }
-  Serial.println("Ending task 2");
-  vTaskDelete(NULL);
 }
