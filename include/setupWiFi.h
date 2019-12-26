ff3ccf419d1c9ff3f3afd70b9b19af4684a56fc1
Anh Pham <anhnnpham@gmail.com>
2019-12-26 11:21:18 +0200

    set up WiFi

--- include/setupWiFi.h
+++ include/setupWiFi.h
@@ -1,14 +1,17 @@
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 
 class setupWiFi
 {
-private:
-    const char *ssid = "ESP32-Access-Point";
-    const char *password = "123456789";
+    private:
+        const char *ssid = "ESP32-Access-Point";
+        const char *password = "123456789";
+        #define LED_BUILTIN 2 // built-in LED
 
-public:
-    setupWiFi(/* args */);
-    ~setupWiFi();
-    IPAddress getIP();
-    void loop();
+    public:
+        setupWiFi(/* args */);
+        ~setupWiFi();
+        IPAddress getIP();
+        void loop();
 };
--- src/setupWiFi.cpp
+++ src/setupWiFi.cpp
@@ -2,7 +2,18 @@
 
 setupWiFi::setupWiFi(/* args */)
 {
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("Configuring access point...");
+
+    // You can remove the password parameter if you want the AP to be open.
     WiFi.softAP(ssid, password);
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
 }
 
 setupWiFi::~setupWiFi()
@@ -13,6 +24,7 @@ void setupWiFi::loop()
 {
     while (1)
     {
+        
     }
 }
 
