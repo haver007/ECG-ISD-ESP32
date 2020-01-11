7eeb3b2f40b66b26b62e6595e2f6ac9cbe5eb409
Maximilian Haverkamp <technohaver@gmail.com>
2020-01-11 18:08:01 +0100

    Added Webserver SD acess still in Work

--- include/webAccess.h
+++ include/webAccess.h
@@ -1,7 +1,27 @@
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <SPI.h>
+#include "mySD.h"
+
 class webAccess
 {
 private:
-    /* data */
+/*Vars*/
+WebServer server;
+bool hasSD = false;
+File uploadFile;
+const char* host = "ecg";
+/* Functions*/
+void returnOK();
+void returnFail(String msg);
+bool loadFromSdCard(String path);
+void handleFileUpload();
+void deleteRecursive(String path);
+void handleDelete();
+void handleCreate();
+void printDirectory();
+void handleNotFound();
+
 public:
     webAccess(/* args */);
     ~webAccess();
