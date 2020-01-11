de837b6ba5d2b0b775574b4a25e7d09834b0666e
Maximilian Haverkamp <technohaver@gmail.com>
2020-01-11 14:13:02 +0100

    Added Func to TurnOff and On WiFiAP

--- src/setupWiFi.cpp
+++ src/setupWiFi.cpp
@@ -2,21 +2,23 @@
 
 setupWiFi::setupWiFi(/* args */)
 {
-    WiFi.softAP(ssid, password);
 }
 
 setupWiFi::~setupWiFi()
 {
 }
 
-void setupWiFi::loop()
-{
-    while (1)
-    {
-    }
-}
-
 IPAddress setupWiFi::getIP()
 {
     return WiFi.softAPIP();
+}
+
+bool setupWiFi::turnOff()
+{
+    WiFi.softAPdisconnect(true);
+}
+
+bool setupWiFi::turnOn()
+{
+    WiFi.softAP(ssid, password);
 }
