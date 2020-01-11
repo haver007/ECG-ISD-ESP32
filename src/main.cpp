1e40543daa4ea8b21b36afbbccaf524592b9c0bc
Maximilian Haverkamp <technohaver@gmail.com>
2020-01-11 18:09:32 +0100

    added Webserver Task

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <FreeRTOS.h>
 #include "setupWiFi.h"
 #include "onScreen.h"
+#include "webAccess.h"
 //declare Tasks
 void taskScreen(void *parameter);
+void taskWebServer(void *parameter);
 
 //Declare Objects
 setupWiFi *crtlWiFi;
 onScreen *crtlScreen;
+webAccess *crtlWeb;
 
 void setup()
 {
@@ -15,9 +18,16 @@ void setup()
   Serial.begin(9600);
   //Setup Objects
   crtlWiFi = new setupWiFi;
+  crtlWiFi->turnOn(); //TODO move to MENU
+      // Give Time to Complete Wifi
+  delay(2000);
+
   crtlScreen = new onScreen;
+  crtlWeb = new webAccess; //This should be done some where in Menu! TODO
+
   // Give Time to Complete Wifi
   delay(2000);
+
   //Create Display Task
   xTaskCreate(
       taskScreen,   /* Task function. */
@@ -26,6 +36,14 @@ void setup()
       NULL,         /* Parameter passed as input of the task */
       1,            /* Priority of the task. */
       NULL);        /* Task handle. */
+//CreateWebTask
+  xTaskCreate(
+      taskWebServer,   /* Task function. */
+      "TaskWebServer", /* String with name of task. */
+      5000,            /* Stack size in bytes. */
+      NULL,            /* Parameter passed as input of the task */
+      1,               /* Priority of the task. */
+      NULL);           /* Task handle. */
 }
 
 void loop()
@@ -37,4 +55,9 @@ void loop()
 void taskScreen(void *parameter)
 {
   crtlScreen->loop();
+}
+
+void taskWebServer(void *parameter)
+{
+  crtlWeb->loop();
 }
