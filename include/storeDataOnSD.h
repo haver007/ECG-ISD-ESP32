23d41ddb48e0cec03adffab64f3130401b6467a4
Henri Peltonen <henri.s.peltonen@gmail.com>
2020-01-13 16:19:12 +0200

    Storage class, in progress, not tested

--- include/storeDataOnSD.h
+++ include/storeDataOnSD.h
@@ -1,10 +1,34 @@
-class storeDataOnSD
-{
-private:
-    /* data */
-public:
-    storeDataOnSD(/* args */);
-    ~storeDataOnSD();
-    void loop();
+#pragma once
+#include <SD.h>
+
+enum class StorageError {
+	None,
+	CannotInitialize,
+	FileSystemError, 
+	TooManyFiles,
+	CanNotOpenFile
 };
 
+enum class StorageState { Idle, Error, Recording };
+
+class Storage {
+	int lastFileIndex = 0;
+	File currentFile;
+	StorageState state_ = StorageState::Idle;
+	StorageError error_ = StorageError::None;
+
+	bool init();
+	void setError(StorageError error);
+	
+
+public:
+	Storage();
+	bool beginRecording();
+	bool endRecording();
+    bool writeRecord(const float data[], uint8_t length);
+    int readRecord(float data[], uint8_t length);
+	void loop();
+	StorageError getError() const;
+	bool clearError();
+	StorageState isRecording(); 
+};
--- src/storeDataOnSD.cpp
+++ src/storeDataOnSD.cpp
@@ -1,16 +1,156 @@
 #include "storeDataOnSD.h"
+#include <SPI.h>
 
-storeDataOnSD::storeDataOnSD(/* args */)
-{
-}
+bool Storage::init() {
+	if (!SD.begin()) {
+        Serial.printf("Storage::init begin error");
+		setError(StorageError::CannotInitialize);
+		return false;
+	}
 
-storeDataOnSD::~storeDataOnSD()
-{
-}
+	if (!SD.exists("/recordings")) {
+		if (!SD.mkdir("/recordings")) {
+	        Serial.printf("Storage::init mkdir /recordings error");
+			setError(StorageError::FileSystemError);
+			return false;
+		}
+	}
 
-void storeDataOnSD::loop()
-{
-    while (1)
-    {
-    }
-}
+	state_ = StorageState::Idle;
+
+	return true;
+};
+
+Storage::Storage(){};
+
+void Storage::setError(StorageError error) {
+	state_ = StorageState::Error;
+	error_ = error;
+};
+
+bool Storage::writeRecord(const float data[], uint8_t length) {
+
+	if (state_ != StorageState::Idle) {
+		return 0;
+	}
+
+	if (!currentFile.write(length)) {
+		setError(StorageError::FileSystemError);
+		return false;
+	}
+
+	if (!currentFile.write((const uint8_t*)data, sizeof(float) * length)) {
+		setError(StorageError::FileSystemError);
+		return false;
+	}
+
+	return true;
+};
+
+int Storage::readRecord(float data[], uint8_t length) {
+
+	if (state_ != StorageState::Idle) {
+		return 0;
+	}
+
+	int dataLength;
+
+	if ((dataLength = currentFile.peek()) == -1) {
+		setError(StorageError::FileSystemError);
+		return 0;
+	}
+
+	if (dataLength >= length) {
+		return -dataLength;
+	}
+
+	if ((dataLength = currentFile.read()) == -1) {
+		setError(StorageError::FileSystemError);
+		return 0;
+	}
+
+	if (!currentFile.read((uint8_t*)data, sizeof(float) * length)) {
+		setError(StorageError::FileSystemError);
+		return 0;
+	}
+
+	return dataLength;
+};
+
+StorageState isRecording() {
+	return StorageState::Recording;
+};
+
+StorageError Storage::getError() const {
+	return error_;
+};
+
+bool Storage::clearError() {
+	if (!init()) {
+		Serial.println("Clear error failed");
+		return false;
+	}
+	state_ = StorageState::Idle;
+	error_ = StorageError::None;
+
+	return true;
+};
+
+bool Storage::beginRecording() {
+	if (state_ == StorageState::Error) {
+		return false;
+	}
+
+	char fileName[23];
+	bool fileFound = false;
+	for (int i = lastFileIndex; i < 10000; i++) {
+
+		snprintf(fileName, 23, "/recordings/%05d.rec", i);
+		if (!SD.exists(fileName)) {
+			fileFound = true;
+			break;
+		}
+	}
+	if (!fileFound) {
+		setError(StorageError::TooManyFiles);
+		return false;
+	}
+	currentFile = SD.open(fileName, FILE_WRITE);
+	if (!currentFile.available()) {
+		setError(StorageError::CanNotOpenFile);
+		return false;
+	}
+	state_ = StorageState::Recording;
+
+	return true;
+};
+
+bool Storage::endRecording() {
+	if (currentFile.available()) {
+		currentFile.close();
+	}
+	state_ = StorageState::Idle;
+	lastFileIndex++;
+
+	return true;
+};
+
+void Storage::loop() {
+	Serial.println("Welcome");
+
+	if (!init()) {
+		Serial.println("First init failed");
+	}
+
+	while (true) {
+		switch (state_) {
+		case StorageState::Idle:
+			break;
+		case StorageState::Error:
+			break;
+		case StorageState::Recording:
+			break;
+		}
+		delay(1);
+	}
+};
